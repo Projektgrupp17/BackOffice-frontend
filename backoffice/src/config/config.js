f173686fbc5de58917f29c3f459647c68eea7c5c
Netanel <netanel@kth.se>
2020-04-28 10:36:41 +0200

    implement config

---
+++ backoffice/src/config/config.js
@@ -0,0 +1,9 @@
+/*
+This file cotains the nessessary information that is needed to create and get 
+env. variables that helps the program run! */
+
+const ENDPOINTAUTH = "https://iot-auth-staging.herokuapp.com/";
+const ENDPOINTBACKEND = "https://backofficerfid.herokuapp.com/";
+
+
+export {ENDPOINTAUTH,ENDPOINTBACKEND};
