627c140c720f188d371a478ba845cb37823a52bd
Netanel <netanel@kth.se>
2020-05-01 15:07:42 +0200

    testing

---
+++ backoffice/src/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const  Button = ({label}) =>{
+    return <div className = "button-style">
+        {label}
+    </div>
+}
+
+export default Button
--- backoffice/src/test/App.test.js
+++ backoffice/src/test/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM, {render} from 'react-dom';
 import Login from '../screen/LoginView/Login';
 import {MemoryRouter} from 'react-router';
 import loginModel from '../model/LoginModel';
+import Button from '../Button';
 
 //Test intitial start screen!
 it('Renders without crashing', () =>{
@@ -10,6 +11,14 @@ it('Renders without crashing', () =>{
     ReactDOM.render(<Login model={loginModel}/>,div);
     ReactDOM.unmountComponentAtNode(div);
 });
+
+
+it("renders and is working", () =>{
+    const div = document.createElement("div");
+    ReactDOM.render(<Button label={test}/>,div)
+    ReactDOM.unmountComponentAtNode(div);
+
+})
 
 //Checks path is correct
 it('redirects to the correct screen on first render', () => {
