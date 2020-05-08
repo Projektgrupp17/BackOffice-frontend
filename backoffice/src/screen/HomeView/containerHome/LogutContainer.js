e434fcd7b59d44f6112023d67d19e8346be8e46f
Netanel <netanel@kth.se>
2020-05-08 20:21:15 +0200

    added comments

--- backoffice/src/screen/HomeView/containerHome/LogutContainer.js
+++ backoffice/src/screen/HomeView/containerHome/LogutContainer.js
@@ -1,3 +1,14 @@
+/**
+ * Date 2020-05-08
+ * 
+ * This js file contains the component for the logout box.
+ * This handles the eventlistner for the logout sequence and
+ * also the display of html elements for the DOM to produce.
+ * 
+ * @author Netanel Avraham Eklind
+ * @version 1.0.0
+ */
+
 import React from 'react'
 import {connect} from 'react-redux'
 import {userLogout} from '../../../model/LoginModel'
@@ -14,13 +25,23 @@ const mapDispatchToProps = dispatch=>{
     }
 }
 
-
+/**
+ * Dispatch the <code> userLogout </code> action and then pushes to go
+ * back in history thus triggering falling back to login screen.
+ * @param {Components} props            the components given to the 
+ * function
+ */
 const logout = (props)=> {
     props.userLogout()
     .then(() => props.history.goBack())
 }
 
-
+/**
+ * Main component function that renders the entire logout box so the user can 
+ * interact with it. It handles a click event on both the <em> yes <em> box and 
+ * <em> no </em> box. 
+ * @param {Components} props 
+ */
 const Logout = props =>{
     return(
         <div id="Logout-box">
