4162dd9ec269d5e3a896e6f9738f62ce9e95d383
Magnus <krummelur@gmail.com>
2020-05-10 21:51:20 +0200

    fix duplicate prop

--- backoffice/src/screen/OrderView/Order.js
+++ backoffice/src/screen/OrderView/Order.js
@@ -112,7 +112,7 @@ class Order extends Component {
                 <button id="Menu" onClick={() => this.handleDisplay()}>
                     <Menu label={"Menu"} display={this.state.displayNavBar} store={this.props.store} />
                 </button>
-                <div className="" className="order-component shown">
+                <div className="order-component">
                     {display}
                 </div>
             </div>
